902fff5482a5a2577ccf4f33c705d85b64519b03
shifeng <li_shifeng@foxmail.com>
2020-04-21 10:31:38 +0800

    使用fs模块读取文件和文件夹内容并返回

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # anydoor
 
-nodeJs 静态资源服务器
+> nodeJs 静态资源服务器
+
+**本项目使用 VSCode 编辑器**
 
 ## 使用说明
 
@@ -9,3 +11,7 @@ nodeJs 静态资源服务器
 * `npx eslint --init` 初始化 eslint 配置
 * `pre-commit` 用于校验 eslint 规则是否通过，若不通过，不允许 git 提交
 * `npx supervisor src/app.js` 启动并监视 node 服务，发生修改时自动重启 node 服务
+
+## 遇到的问题
+
+* 使用 createReadStream 读取文件内容时，中文注释显示乱码（选择 utf8bom 编码保存文件可解决）
--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-const http = require('http')
+﻿const http = require('http')
 const chalk = require('chalk')
+const path = require('path')
+const fs = require('fs')
 const conf = require('./config/defaultConfig')
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World !')
+  const filePath = path.join(conf.root, req.url)
+
+  fs.stat(filePath, (err, stats) => {
+    if (err) {
+      res.statusCode = 404
+      res.setHeader('Content-Type', 'text/plain')
+      res.end(`${filePath} is not a directory or file`)
+      return
+    }
+
+    if (stats.isFile()) {
+      // 访问的是文件
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'text/plain')
+      fs.createReadStream(filePath).pipe(res)
+    } else if (stats.isDirectory()) {
+      // 访问的是文件夹
+      fs.readdir(filePath, (err, files) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/plain')
+        res.end(files.join(','))
+      })
+    }
+  })
 })
 
 server.listen(conf.port, conf.hostname, () => {
